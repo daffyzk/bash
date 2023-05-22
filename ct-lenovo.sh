4d3609e934e2ef43f862ef174e7964b8e94861a6
maurogarciac <maurogarciacoto@gmail.com>
2023-05-22 18:32:16 -0300

    made script funnier

--- ct-lenovo.sh
+++ ct-lenovo.sh
@@ -2,14 +2,24 @@
 
 echo "beep boop connecting to desktop"
 
-xrandr --output DisplayPort-4 --mode 1920x1080 --rate 120.00 --left-of DisplayPort-2
-xrandr --output DisplayPort-2 --mode 1920x1080 --rate 60.00 --left-of eDP
+print_funny(){
+    if [[ $1 -gt 7 ]]
+    then 
+        echo "eldenring connection..."
+    else
+        echo "attempting connection..."
+    fi
+}
+
+for i in {1..2} 
+do
+    print_funny $(($RANDOM % 10 + 1 ))
+    xrandr --output DisplayPort-4 --mode 1920x1080 --rate 120.00 --left-of DisplayPort-2
+    xrandr --output DisplayPort-2 --mode 1920x1080 --rate 60.00 --left-of eDP
+done
 
 setxkbmap -layout us
 
-xrandr --output DisplayPort-4 --mode 1920x1080 --rate 120.00 --left-of DisplayPort-2
-xrandr --output DisplayPort-2 --mode 1920x1080 --rate 60.00 --left-of eDP
-
-echo "connected"
+echo "connected!!!! successs!!! yeaaahhhhh!! lets gooooo!!!"
 
 exit 0
