0b959c19152db2172ab5ed942a857674766e941f
Mauro Garcia Coto <maurogarciacoto@gmail.com>
2023-08-21 02:14:54 +0300

    fixed dc script for arch

--- dc-all-outputs.sh
+++ dc-all-outputs.sh
@@ -3,6 +3,9 @@
 echo "disconnecting from both monitors"
 
 xrandr --output HDMI-A-0 --off
+xrandr --output DP-1 --off
+xrandr --output DP-3 --off
+xrandr --output DP-5 --off
 xrandr --output DisplayPort-0 --off
 xrandr --output DisplayPort-1 --off
 xrandr --output DisplayPort-2 --off
